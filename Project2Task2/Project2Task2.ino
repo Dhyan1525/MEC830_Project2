0da9ca9cb039c77878d2bde74a13793076af6e14
Dhyan1525 <118860927+Dhyan1525@users.noreply.github.com>
2022-11-24 12:39:39 -0500

    Update Project2Task2.ino

--- Project2Task2/Project2Task2.ino
+++ Project2Task2/Project2Task2.ino
@@ -8,26 +8,43 @@ void setup(){
 }
 
 void loop(){
-  
+  delay(5000);
+fd1();
+      delay(1000);
+rt();
+      delay(500);
+fd1();
+      delay(1000);
+lt();
+      delay(500);
+fd2();
+ delay(100000);  
+}
+
+void fd1() {
  for (int fd = 0; fd <= 12990; fd++) {
       steppermotor1.step(-1);
       steppermotor2.step(1);
       }
+}
+
+void fd2() {
+ for (int fd = 0; fd <= 13800; fd++) {
+      steppermotor1.step(-1);
+      steppermotor2.step(1);
+      }
+}
+
+void rt(){
  for (int rt = 0; rt <= 1145; rt++) {
       steppermotor1.step(-1);
       steppermotor2.step(-1);
       }
- for (int fd = 0; fd <= 12990; fd++) {
-      steppermotor1.step(-1);
-      steppermotor2.step(1);
-      }
- for (int lt = 0; lt <= 1145; lt++) {
+}
+
+void lt(){
+   for (int lt = 0; lt <= 1223; lt++) {
       steppermotor1.step(1);
       steppermotor2.step(1);
       }
- for (int fd = 0; fd <= 13900; fd++) {
-      steppermotor1.step(-1);
-      steppermotor2.step(1);
-      }
- delay(100000);  
 }
